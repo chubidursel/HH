a64824c3a009f99c983be2b0111ece690620565b
chubidursel <baurimk21@gmail.com>
2023-02-24 22:44:26 -0800

    add networks

--- contracts/Demo.sol
+++ contracts/Demo.sol
@@ -1,13 +1,17 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-import "hardhat/console.sol";
 
-contract Lock {
+contract Demo {
     uint public num;
 
     function setNum(uint _num)public{
         num = _num;
-        console.log("Shalom");
+    }
+
+    string public str;
+
+    function setString(string calldata _str)public {
+        str = _str;
     }
 }
---
+++ deployment/deployDemo.ts
@@ -0,0 +1,23 @@
+import { ethers } from "hardhat";
+
+// npx hardhat run --network goerli deployment/deployUSDT.ts
+
+async function main() {
+  console.log("Start Deploying....")
+
+  const [deployer] = await ethers.getSigners();
+
+  const Contract = await ethers.getContractFactory("Demo");
+  const contract = await Contract.deploy();
+
+  await contract.deployed();
+
+  console.log("👨 The owner of smart contract is: ", deployer.address);
+  console.log(`🔥 SC Deployed! Address: ${contract.address}`);
+}
+
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
