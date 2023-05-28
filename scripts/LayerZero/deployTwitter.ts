48529d843777b1eed2ca461fcc10122fd45990a4
chubidursel <baurimk21@gmail.com>
2023-05-28 09:33:26 -0700

    update bridge

---
+++ scripts/LayerZero/deployTwitter.ts
@@ -0,0 +1,139 @@
+import { ethers } from "hardhat";
+
+// npx hardhat clean && npx hardhat compile
+// npx hardhat run --network mumbai scripts/LayerZero/deployTwitter.ts
+ // npx hardhat verify --network mumbai 0x061C9f4e28175BA70f5fc62a885820973E92f0b8 0xf69186dfBa60DdB133E91E9A4B5673624293d8F8  👳
+
+
+async function main() {
+  console.log("Starting 🏃")
+
+  const Demo = await ethers.getContractFactory("CrossChainNFT");
+
+    const [deployer] = await ethers.getSigners();
+    console.log("👷 Account: ", deployer.address)
+    const gasPrice = await deployer.getGasPrice()
+    console.log("📄 getGasPrice(): ", gasPrice)
+   
+// POLYGON -> "0xf69186dfBa60DdB133E91E9A4B5673624293d8F8" || chainId: 10109
+  const demo = await Demo.deploy("0xf69186dfBa60DdB133E91E9A4B5673624293d8F8")
+  
+  const tx = await demo.deployed();
+
+  const transaction = await demo.deployTransaction.wait();
+  const gasUsed = transaction.gasUsed;
+
+
+  console.log('$$$$ Gas used:', gasUsed.toString());
+  const transactionCost = gasUsed.mul(gasPrice);
+  console.log("Transaction cost in Ether:", ethers.utils.formatEther(transactionCost));
+
+  console.log('SC ADDRESS -> ', demo.address);
+  console.log(`🏁 FINISHED 🏁`);
+}
+
+ 
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+
+
+
+
+
+//---------------- USEFUL CODE --------------
+
+
+ // function sleep(milliseconds : number) {
+  //   const date = Date.now();
+  //   let currentDate = null;
+  //   do {
+  //     currentDate = Date.now();
+  //   } while (currentDate - date < milliseconds);
+  // }
+  // sleep(5000)
+
+      //----------------- EVENTS --------------
+// console.log("📢 EVENTS")
+// //const eventsAll = await contract.queryFilter("*" as any, 0,  (await ethers.provider.getBlock(await ethers.provider.getBlockNumber())).timestamp;)
+// const blockNumber = await ethers.provider.getBlockNumber();
+
+// // Get all events from the contract from block 0 to the latest block
+// const eventsAddEmployee = await contract.queryFilter(
+//   contract.filters["AddEmployee(address,uint256)"](),
+//   0,
+//   blockNumber
+// );
+// console.log("Amount Evetns: ", eventsAddEmployee.length)
+
+
+// const eventsStartStream = await contract.queryFilter(
+//   contract.filters.StreamCreated(),
+//   0,
+//   blockNumber
+// );
+
+// console.log("Stream Created Evetns: ", eventsStartStream[0].args)
+
+// // let eventFilterFinish = contract.filters["StreamFinished(address,uint256,uint256)"];
+// // let eventsFinishStream = await contractFactory.queryFilter(eventFilterFinish);
+// // console.log("📢 EVENTS FINISH: ", eventsFinishStream.length)
+
+
+  //----------------- EVENTS --------------
+// console.log("📢 EVENTS" ,  contract.filters.AddEmployee())
+// contractUSDT.on("Transfer", (to, amount, from) => {
+//   console.log("📢🟡 EVENTS", to, amount, from);
+// });
+
+//----------------- INFO --------------
+// const info1 = await contract.totalAmountEmployee();
+// const info2 = await contract.allEmployee(acc1.address);
+// console.log("📄 INFO [ amount employee ] ➡️  ", info1.toString())
+// console.log("📄 INFO [ info employee #1 ] ➡️  ", info2.flowRate)
+
+// const infoToken1 = await contractUSDT.balanceOf(deployer.address);
+// console.log("📄🟡 INFO [ Balance ] ➡️  ", infoToken1.toString())
+
+// console.log("📄📕 INFO [ blocknumber ] ➡️  ",  await ethers.provider.getBlockNumber())
+// console.log("📄📕 INFO [ acc1 = eth ] ➡️  ",  await ethers.provider.getBalance(deployer.address))
+// console.log("📄📕 INFO [ gas price ] ➡️  ",  ethers.utils.formatEther(await ethers.provider.getGasPrice()), "gwei")
+
+
+// ????????? CALCULATE token limit to add new Employee
+// const totalAmountEmployees = (await contract.amountEmployee()).toNumber();
+// const cRrate = (await contract.commonRateAllEmployee()).toNumber();
+// const hoursLimit = (await contract.hoursLimitToAddNewEmployee()).toNumber();
+// const res = (totalAmountEmployees + 1 ) * (cRrate + 10) * hoursLimit;
+//     console.log(`📈 Calculation Math to add new employee:  ${totalAmountEmployees + 1} * ${cRrate + 10} * ${hoursLimit} = ${res} `)
+// // ????????? CALCULATE token limit to Stream All 
+//console.log("---------📈 BUFFER INFO 📈------------")
+// const totalAmountEmployees2 = (await contract.amountEmployee()).toNumber();
+// const cRrate2 = (await contract.commonRateAllEmployee()).toNumber();
+// const hoursLimit2 = (await contract.hoursLimitToAddNewEmployee()).toNumber();
+// const res2 = totalAmountEmployees2 * cRrate2 * hoursLimit2;
+
+// const scBal2 = (await contract.balanceContract()).toNumber();
+// console.log(`📈 Calculation Math to add new employee:  ${totalAmountEmployees2} * ${cRrate2} * ${hoursLimit2} = ${res2} `)
+// console.log("📈Valid TO ADD // Balance: ", scBal2)
+// console.log("📈 Can I add new Employee ", (scBal2 > res2))
+// console.log("📈 Time different", (scBal2 - res2))
+
+
+
+
+
+
+// #1 Check gas price
+// #2 Estimae gas price
+// #3 Balance are enough? 
+// 
+
+const add1 = [1,2,3]
+
+
+function doTx(){
+  // SET UP MINIMUM GAS LIMMIT AND GAS PRICE
+ //Ehters send tx
+}
